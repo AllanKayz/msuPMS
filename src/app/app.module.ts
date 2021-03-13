f97ca3f721b8259add9133a5cefb894cd8eabab6
Allan Kanyemba <allankanyemba@gmail.com>
2021-03-13 14:04:47 +0200

    Made Leaflet Connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/findparkingspace/findparkingspace.component.ts
+++ src/app/findparkingspace/findparkingspace.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-findparkingspace',
   templateUrl: './findparkingspace.component.html',
   styleUrls: ['./findparkingspace.component.css']
 })
-export class FindparkingspaceComponent implements OnInit {
+
+@ViewChild('map')
+
+export class FindparkingspaceComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    const map = L.map('map').setView([51.503, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Allan Kanyemba Things'
+    }).addTo(map);
+
+    L.marker([51.5, -0.09]).addTo(map);
   }
+
+  ngAfterViewInit() {
+  }
+
+
+  /*private map: L.Map;
+
+  @ViewChild('map')
+  private mapContainer: ElementRef<HTMLElement>;
+
+  ngAfterViewInit() {
+    const map = new L.Map(this.mapContainer.nativeElement).setView(
+      [51.503, -0.09], 13
+    );
+
+    L.tileLayer()
+  }*/
 
 }
